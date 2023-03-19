9dd73aa30740a10954ec9a7b8155b08c375667a8
Sheron Makola <2344817@students.wits.ac.za>
2023-03-19 03:42:38 +0200

    project setup

---
+++ wits_overflow/android/app/src/main/kotlin/com/example/wits_overflow/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wits_overflow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ wits_overflow/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Wits-Overflow'),
+      ),
+      body: const Center(
+        child: Text('Homepage'),
+      ),
+    ),
+  ));
+}
