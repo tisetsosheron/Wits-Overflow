04ae08c0dd9d4e41131501d903d8e71b1df6e68a
Kgosi8 <2489247@students.wits.ac.za>
2023-03-23 21:32:31 +0000

    Sign in authentication using Firebase updated, Logged in as a user updated in the home page profile

--- wits_overflow/lib/main.dart
+++ wits_overflow/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:wits_overflow/signin.dart';
+import 'package:wits_overflow/main_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: MainPage(),
     );
   }
 }
