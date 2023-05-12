34fbd47f0d10a2dc2903f423b41e73683a6047ee
Sheron Makola <2344817@students.wits.ac.za>
2023-05-12 18:09:35 +0200

    login_or_register test

--- lib/login_or_register.dart
+++ lib/login_or_register.dart
@@ -3,15 +3,23 @@ import 'package:wits_overflow/register.dart';
 import 'package:wits_overflow/signin.dart';
 
 class LoginOrRegister extends StatefulWidget {
-  const LoginOrRegister({super.key});
+  final bool showLogin;
+
+  const LoginOrRegister({Key? key, required this.showLogin}) : super(key: key);
 
   @override
   State<LoginOrRegister> createState() => _LoginOrRegisterState();
 }
 
 class _LoginOrRegisterState extends State<LoginOrRegister> {
-  //Showing the Login Page
   bool showLogin = true;
+
+  @override
+  void initState() {
+    super.initState();
+    showLogin = widget.showLogin;
+  }
+
   void togglePages() {
     setState(() {
       showLogin = !showLogin;
@@ -20,14 +28,14 @@ class _LoginOrRegisterState extends State<LoginOrRegister> {
 
   @override
   Widget build(BuildContext context) {
-    //if user is registered show login page
     if (showLogin) {
       return LoginPage(
+        key: const Key('toggle-pages'),
         onTap: togglePages,
       );
-      //else  show register page
     } else {
       return Register(
+        key: const Key('toggle-pages'),
         onTap: togglePages,
       );
     }
---
+++ test/login_or_register_test.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:wits_overflow/login_or_register.dart';
+import 'package:wits_overflow/register.dart';
+import 'package:wits_overflow/signin.dart';
+
+void main() {
+  testWidgets('should show LoginPage when showLogin is true',
+      (WidgetTester tester) async {
+    await tester
+        .pumpWidget(const MaterialApp(home: LoginOrRegister(showLogin: true)));
+
+    expect(find.byType(LoginPage), findsOneWidget);
+    expect(find.byType(Register), findsNothing);
+  });
+
+  // testWidgets('should show Register when showLogin is false',
+  //     (WidgetTester tester) async {
+  //   await tester
+  //       .pumpWidget(MaterialApp(home: LoginOrRegister(showLogin: false)));
+
+  //   expect(find.byType(Register), findsOneWidget);
+  //   expect(find.byType(LoginPage), findsNothing);
+
+  //   final registerWidget = tester.widget<Register>(find.byType(Register));
+  //   expect(registerWidget, isNotNull);
+  // });
+
+  // testWidgets(
+  //     'should toggle between LoginPage and Register when onTap is called',
+  //     (WidgetTester tester) async {
+  //   await tester
+  //       .pumpWidget(MaterialApp(home: LoginOrRegister(showLogin: true)));
+
+  //   expect(find.byType(LoginPage), findsOneWidget);
+  //   expect(find.byType(Register), findsNothing);
+
+  //   await tester.tap(find.byKey(const Key('toggle-pages')));
+  //   await tester.pump();
+
+  //   expect(find.byType(Register), findsOneWidget);
+  //   expect(find.byType(LoginPage), findsNothing);
+
+  //   await tester.tap(find.byKey(const Key('toggle-pages')));
+  //   await tester.pump();
+
+  //   expect(find.byType(LoginPage), findsOneWidget);
+  //   expect(find.byType(Register), findsNothing);
+  // });
+}
