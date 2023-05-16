c10b3fb2d7a94f743377c0ad675bb094e23edc54
slall100 <2107168@students.wits.ac.za>
2023-05-16 13:00:19 +0200

    new tests

--- test/post_answers_test.dart
+++ test/post_answers_test.dart
@@ -1,30 +1,55 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:wits_overflow/PostAnswers/post_answers.dart';
 
 void main() {
-  // Mock dependencies or provide necessary setup
-
-  group('_AnswersState', () {
-    // late _AnswersState answersState;
+  group('Answers Widget', () {
+    late Answers answersWidget;
+    final String questionId = 'example_question_id';
 
     setUp(() {
-      // Initialize the _AnswersState instance
-      //  answersState = _AnswersState();
+      answersWidget = Answers(questionId: questionId);
     });
 
-    test('PostAnswer should add the answer to Firestore', () async {
-      // Mock necessary dependencies (e.g., Firestore)
-      // Setup the necessary data for testing
+    testWidgets('post', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(home: answersWidget));
 
-      // Call the method being tested
-      //  await answersState.PostAnswer();
+      // Mock necessary dependencies (e.g., FirebaseAuth, FirebaseFirestore)
+      // Setup necessary data for testing
+
+      // Enter text in the TextField
+      await tester.enterText(find.byType(TextField), 'Example answer');
+
+      // Tap the send IconButton
+      await tester.tap(find.byIcon(Icons.send));
+
+      // Wait for the answer to be posted
+      await tester.pumpAndSettle();
 
       // Assert the expected outcome
-      // Check if the answer is added to Firestore successfully
+      // Verify if the answer is added to Firestore correctly
       // You can use mock objects or verify the function calls
 
       // Verify any other expected behavior
     });
 
-    // Add more tests for other methods or behaviors in _AnswersState class
+    testWidgets('Answer list is displayed correctly',
+        (WidgetTester tester) async {
+      //await tester.pumpWidget(MaterialApp(home: answersWidget));
+
+      // Mock necessary dependencies (e.g., FirebaseAuth, FirebaseFirestore)
+      // Setup necessary data for testing (e.g., a list of answers)
+
+      // Wait for the FutureBuilder to complete
+      await tester.pumpAndSettle();
+
+      // Assert the expected outcome
+      // Verify if the answer list is displayed correctly
+      // You can use mock objects or verify the function calls
+
+      // Verify any other expected behavior
+    });
+
+    // Add more tests for other behaviors or functionality of the Answers widget
   });
 }
