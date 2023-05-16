fdde6f4424f824e216c8186385aca0f01fc9586e
slall100 <2107168@students.wits.ac.za>
2023-05-16 12:36:44 +0200

    new tests

---
+++ test/post_answers_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // Mock dependencies or provide necessary setup
+
+  group('_AnswersState', () {
+    // late _AnswersState answersState;
+
+    setUp(() {
+      // Initialize the _AnswersState instance
+      //  answersState = _AnswersState();
+    });
+
+    test('PostAnswer should add the answer to Firestore', () async {
+      // Mock necessary dependencies (e.g., Firestore)
+      // Setup the necessary data for testing
+
+      // Call the method being tested
+      //  await answersState.PostAnswer();
+
+      // Assert the expected outcome
+      // Check if the answer is added to Firestore successfully
+      // You can use mock objects or verify the function calls
+
+      // Verify any other expected behavior
+    });
+
+    // Add more tests for other methods or behaviors in _AnswersState class
+  });
+}
