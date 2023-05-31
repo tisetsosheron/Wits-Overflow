60453e7199713545d6fb712615ce8cd20be88f53
slall100 <127601732+slall100@users.noreply.github.com>
2023-05-31 22:24:09 +0200

    Update user_model.dart

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,18 +1,21 @@
 class UserModel {
-  String? name;
-  String? email;
-  String? password;
-  String? location;
-  String? image;
+  String? name;       // Represents the user's name
+  String? email;      // Represents the user's email address
+  String? password;   // Represents the user's password
+  String? location;   // Represents the user's location
+  String? image;      // Represents the URL or path to the user's image
 
-  UserModel();
+  UserModel();        // Default constructor for the UserModel class
+
+  // Constructor to create a UserModel object from a JSON map
   UserModel.fromJson(Map<String, dynamic> json) {
-    name = json['name'];
-    email = json['email'];
-    password = json['password'];
-    location = json['location'];
-    image = json['image'];
+    name = json['name'];           // Retrieves the 'name' field value from the JSON map
+    email = json['email'];         // Retrieves the 'email' field value from the JSON map
+    password = json['password'];   // Retrieves the 'password' field value from the JSON map
+    location = json['location'];   // Retrieves the 'location' field value from the JSON map
+    image = json['image'];         // Retrieves the 'image' field value from the JSON map
   }
 
-  get obs => null;
+  get obs => null;  // This getter method returns null. It might be an incomplete or placeholder implementation.
 }
+
