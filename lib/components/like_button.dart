89b2e87e9ccedaafcb4ef4eb0ef6fddf8a6fa936
slall100 <127601732+slall100@users.noreply.github.com>
2023-05-31 22:31:42 +0200

    Update like_button.dart

--- lib/components/like_button.dart
+++ lib/components/like_button.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 
 class LikeButton extends StatelessWidget {
-  final bool isLiked;
-  void Function()? onTap;
+  final bool isLiked;   // Represents whether the button is in a liked state or not
+  void Function()? onTap;  // Callback function to be executed when the button is tapped
 
   LikeButton({super.key, required this.isLiked, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTap,
+      onTap: onTap,   // Assigns the onTap callback function to the GestureDetector's onTap event
       child: Icon(
         Icons.thumb_up,
-        color: isLiked ? Colors.blue : Colors.grey,
+        color: isLiked ? Colors.blue : Colors.grey,  // Sets the color based on the isLiked flag
         size: 20,
       ),
     );
   }
 }
+
