9bf1d845987c210e8a18d6cdb5acc3645fd9e524
slall100 <127601732+slall100@users.noreply.github.com>
2023-05-31 22:30:45 +0200

    Update dislike_button.dart

--- lib/components/dislike_button.dart
+++ lib/components/dislike_button.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 
 class DislikeButton extends StatelessWidget {
-  final bool isDisliked;
-  void Function()? onTap;
+  final bool isDisliked;   // Represents whether the button is in a disliked state or not
+  void Function()? onTap;  // Callback function to be executed when the button is tapped
 
   DislikeButton({super.key, required this.isDisliked, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTap,
+      onTap: onTap,   // Assigns the onTap callback function to the GestureDetector's onTap event
       child: Icon(
         Icons.thumb_down,
         size: 20,
-        color: isDisliked ? Colors.blue : Colors.grey,
+        color: isDisliked ? Colors.blue : Colors.grey,  // Sets the color based on the isDisliked flag
       ),
     );
   }
 }
+
