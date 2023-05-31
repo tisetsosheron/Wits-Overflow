a6483567d6ca6c0c0be08fa192d0cbb4d9280027
slall100 <127601732+slall100@users.noreply.github.com>
2023-05-31 22:18:18 +0200

    Update comments.dart

--- lib/model/comments.dart
+++ lib/model/comments.dart
@@ -1,23 +1,26 @@
-class comments {
-  String? comment;
-  DateTime? created;
-  String? username;
-  List<String>? Likes;
-  List<String>? Dislikes;
+class Comments {
+  String? comment;         // Represents the comment text
+  DateTime? created;       // Represents the date and time when the comment was created
+  String? username;        // Represents the username of the person who made the comment
+  List<String>? likes;     // Represents the list of usernames who liked the comment
+  List<String>? dislikes;  // Represents the list of usernames who disliked the comment
 
-  comments();
+  Comments();              // Default constructor for the Comments class
 
+  // Converts the Comments object to a JSON format
   Map<String, dynamic> toJson() => {
         'comment': comment,
         'username': username,
         'created': created,
-        'Likes': Likes,
-        'Dislikes': Dislikes
+        'likes': likes,
+        'dislikes': dislikes
       };
-  comments.fromSnapshot(snapshot)
-      : comment = snapshot.data()['comment'],
-        username = snapshot.data()['username'],
-        created = snapshot.data()['created'].toDate(),
-        Likes = snapshot.data()['Likes'],
-        Dislikes = snapshot.data()['Dislikes'];
+
+  // Constructor to create a Comments object from a snapshot (presumably from a database query)
+  Comments.fromSnapshot(snapshot)
+      : comment = snapshot.data()['comment'],                // Retrieves the 'comment' field value from the snapshot
+        username = snapshot.data()['username'],              // Retrieves the 'username' field value from the snapshot
+        created = snapshot.data()['created'].toDate(),       // Retrieves the 'created' field value from the snapshot and converts it to a DateTime object using the toDate() method
+        likes = snapshot.data()['likes'],                    // Retrieves the 'likes' field value from the snapshot
+        dislikes = snapshot.data()['dislikes'];              // Retrieves the 'dislikes' field value from the snapshot
 }
